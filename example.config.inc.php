e6fb5bd0bf1e20d0818a45d6fb9a3eb42a0a5834
Nicholas Alipaz <nalipaz@advanstar.com>
2013-05-21 15:37:13 -0700

    better documentation on the configurable settings and doxygen style comments.

--- example.config.inc.php
+++ example.config.inc.php
@@ -1,64 +1,80 @@
 <?php
 /**
+ * @file
+ * An example config file to be used as a starting point in your config.
+ * 
  * This example configuration file is all you need to start. Make a copy
  * as config.inc.php and the file will be used by hook.php to set up the git 
  * process and post-git commands (modifying folder security for apache).
  */
 
-// Configure admin email for error messages.
+/**
+ * Configuration options
+ * 
+ * @param string $email
+ *   Configure admin email for error messages.
+ * @param string $git
+ *   Location of git binary.
+ * @param boolean $enableLogging
+ *   Whether or not to enable logging.
+ * @param string $logDirectory
+ *   The log directory used in all branches when not specified in the branch 
+ *   settings.
+ * @param string $logFileName
+ *   The log file name use in all branches when not specified in the branch and
+ *   when the log entry doesn't match a branch.
+ * @param array $branches
+ *   An array of arrays representing each site/repository.
+ *     Each array has a number of configurable keys.
+ *     - branchName: Can be a branch name or a tag prefix, dependent on value
+ *       of branchType.
+ *         - Branch; the branch to deploy. 'stage' branch used on staging 
+ *           server, dev for dev, prod for prod servers (stage).
+ *         - Tag Prefix: the tag prefix to look for when determining where to
+ *           deploy. 'prod' would match 'prod-20130201-1' as an example.
+ *     - branchType: Either branch or tag.
+ *         - branch: Indicates that a specific branch will be used to make the
+ *           deployments.  Examples: master, prod, stage, etc.
+ *         - tag: Indicates that a tag prefix will be used to make the 
+ *           deployments.  Example: 'stage' would mean that any tag prefixed
+ *           with 'stage-' would deploy to the stage server, as in 
+ *           'stage-2013-05-01-1'.
+ *     - branchTitle: Name of branch used in logging.
+ *     - logFileName: Can be used to specify a log file per site.
+ *     - logDirectory: Can be used to specify a log directory per site.
+ *     - gitFolder: The directory where the git repository will be held and
+ *       git commands will be executed.
+ *     - docRoot: The document root for the application, files from the git
+ *       repo will be copied here using rsync.
+ *     - owner: The linux user whom should own the files in the document root.
+ *     - gitURL: The remove URL of the Git project, example:
+ *       https://github.com/nalipaz/GitHubHook
+ *     - allowedEmails: Optional array of email addresses.
+ * @param array githubIPs
+ *   An array of allowable IPs which can execute this script for deployment.
+ */
+
 $email = '';
-
-// Location of git binary.
 $git = '/usr/bin/git';
-
-// Enable logging.
 $enableLogging = TRUE;
-
-// The log settings use for all branches if not specified otherwise in the branch settings.
 $logDirectory = 'log';
 $logFileName = 'hook';
 
-// Branch settings.
 $branches = array(
   array(
-    'branchName' => 'master', // The branch to deploy. 'stage' branch used on staging server, dev for dev, prod for prod servers (stage).
-    'branchTitle' => 'Development', // Just used in logging (staging).
-    'logFileName' => 'example.com', // Log filename to use, when empty the log settings near the top of this file are used.
-    'logDirectory' => '', // Log directory to use, when empty the log settings near the top of this file are used.
-    'gitFolder' => '/var/www/example.com', // The folder for the site that we're deploying (/var/www/MyWebsite).
+    'branchName' => '',
+    'branchTitle' => '',
+    'branchType' => '',
+    'logFileName' => '',
+    'logDirectory' => '',
+    'gitFolder' => '',
     'docRoot' => '',
     'owner' => '',
-    'gitURL' => 'https://github.com/username/example', // The remote URL of the Git project (https://github.com/ajbogh/GitHubHook)
-    'allowedEmails' => array( // Optional, can be blank array.
-//      'email@example.com',
-    ),
+    'gitURL' => '',
+    'allowedEmails' => array(),
   ),
-  array(
-    'branchName' => 'prod', // The tagname identifier to deploy, example: prod-2013-02-01
-    'branchType' => 'tag', // The type to look for, branch is assumed, but you could also use tag
-    'branchTitle' => 'Production', // Just used in logging (staging).
-    'logFileName' => 'example.com', // Log filename to use, when empty the log settings near the top of this file are used.
-    'logDirectory' => '', // Log directory to use, when empty the log settings near the top of this file are used.
-    'gitFolder' => '/var/www/example.com', // The folder for the site that we're deploying (/var/www/MyWebsite).
-    'docRoot' => '',
-    'owner' => '',
-    'gitURL' => 'https://github.com/username/example', // The remote URL of the Git project (https://github.com/ajbogh/GitHubHook)
-    'allowedEmails' => array( // Optional, can be blank array.
-//      'email@example.com',
-    ),
-  ),
-//  array(
-//    'branchName' => '',
-//    'branchTitle' => '',
-//    'logFileName' => '',
-//    'logDirectory' => '',
-//    'gitFolder' => '',
-//    'gitURL' => '',
-//    'allowedEmails' => array(),
-//  ),
 );
 
-// An array of IPs that can run the deployment.
 $githubIPs = array(
   '207.97.227.253',
   '50.57.128.197',
