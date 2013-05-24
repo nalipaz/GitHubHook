16bec9194e0652a845468dae5390afed249e0f98
Nicholas Alipaz <nalipaz@advanstar.com>
2013-05-24 10:01:32 -0700

    testing new rsync command.

--- rsync-data.sh
+++ rsync-data.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-rsync --delete -avze --filter="-rsp_/.git" --filter="-rsp_/.gitignore" --filter="-rsp_/drushrc.php" --filter="-rsp_/files" --filter="-rsp_/modules/development" --filter="-rsp_/private" --filter="-rsp_/README.md" --filter="-rsp_/settings.php" $1 $2;
+rsync --delete -avze \
+--filter="-rsp_/.git" \
+--filter="-rsp_/.gitignore" \
+--filter="-rsp_/drushrc.php" \
+--filter="-rsp_/files" \
+--filter="-rsp_/modules/development" \
+--filter="-rsp_/private" \
+--filter="-rsp_/README.md" \
+--filter="-rsp_/settings.php" \
+$1 $2;
