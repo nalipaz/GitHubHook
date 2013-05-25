c90951c141dbd153dace1f19aa1766b4f69b2ff7
Nicholas Alipaz <nalipaz@advanstar.com>
2013-05-25 04:36:57 -0700

    only do backups on tags to non-dev servers.

--- class.GitHubHook.php
+++ class.GitHubHook.php
@@ -287,9 +287,9 @@ class GitHubHook {
     // @todo: figure out how to get proper log messages regarding domain rather
     // than hostmaster, then do automated rollback if there is an error.
     if ($branch['domain']) {
-  //    $output[] = trim(shell_exec('sudo -u ' . $branch['owner'] . ' TERM=dumb /usr/bin/drush --verbose @hostmaster hosting-task @' . $branch['domain'] . ' backup 2>&1'));
-  //    $output[] = trim(shell_exec('sudo -u ' . $branch['owner'] . ' TERM=dumb /usr/bin/drush --verbose @' . $branch['domain'] . ' updatedb 2>&1'));
-  //    $output[] = trim(shell_exec('sudo -u ' . $branch['owner'] . ' TERM=dumb /usr/bin/drush --verbose @hostmaster hosting-task @' . $branch['domain'] . ' verify 2>&1'));
+//      $output[] = trim(shell_exec('sudo -u ' . $branch['owner'] . ' TERM=dumb /usr/bin/drush --verbose @hostmaster hosting-task @' . $branch['domain'] . ' backup 2>&1'));
+//      $output[] = trim(shell_exec('sudo -u ' . $branch['owner'] . ' TERM=dumb /usr/bin/drush --verbose @' . $branch['domain'] . ' updatedb 2>&1'));
+//      $output[] = trim(shell_exec('sudo -u ' . $branch['owner'] . ' TERM=dumb /usr/bin/drush --verbose @hostmaster hosting-task @' . $branch['domain'] . ' verify 2>&1'));
       if ($branch['type'] === 'tags') {
         $backup_output = trim(shell_exec('sudo -u ' . $branch['owner'] . ' TERM=dumb /usr/bin/drush -v @' . $branch['domain'] . ' provision-backup 2>&1'));
         $output[] = $backup_output;
